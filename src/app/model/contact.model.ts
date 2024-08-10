903fa9227e8dd4b78c4a53264fe1b7c97cbb0171
joheiss <josef.heiss@jovisco.de>
2024-08-10 18:17:05 +0200

    after section 12

--- src/app/model/contact.model.ts
+++ src/app/model/contact.model.ts
@@ -6,15 +6,14 @@ export class Contact {
   public contactEmail: string;
   public subject: string;
   public message: string;
-  
-  constructor(contactId?: string,contactName?: string,contactEmail?: string,
-    subject?: string,message?: string){
-        this.contactId = contactId || '';
-        this.contactName = contactName || '';
-        this.contactEmail = contactEmail || '';
-        this.subject = subject || '';
-        this.message = message || '';
-        
+
+  constructor(contactId?: string, contactName?: string, contactEmail?: string,
+    subject?: string, message?: string) {
+    this.contactId = contactId || '';
+    this.contactName = contactName || '';
+    this.contactEmail = contactEmail || '';
+    this.subject = subject || '';
+    this.message = message || '';
   }
 
 }
